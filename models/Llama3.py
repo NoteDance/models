1f3481697366baaec7eab08605e47d486fc22974
NoteDance <63648431+NoteDance@users.noreply.github.com>
2024-06-13 15:34:38 +0800

    Update Llama3.py

--- models/Llama3.py
+++ models/Llama3.py
@@ -1,4 +1,4 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
+# Copyright (c) NoteDance, Inc. and affiliates.
 # This software may be used and distributed in accordance with the terms of the Llama 3 Community License Agreement.
 import tensorflow as tf
 from tensorflow.keras.layers import Embedding,Dense
